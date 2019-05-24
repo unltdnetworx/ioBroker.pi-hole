8ffb68d9ac4cc8df30c579314ba27602aec5fa01
Michael Schuster <development@unltd-networx.de>
2019-05-24 17:27:37 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"pi-hole adapter settings": {
+		"en": "Adapter settings for pi-hole",
+		"de": "Adaptereinstellungen für pi-hole",
+		"ru": "Настройки адаптера для pi-hole",
+		"pt": "Configurações do adaptador para pi-hole",
+		"nl": "Adapterinstellingen voor pi-hole",
+		"fr": "Paramètres d'adaptateur pour pi-hole",
+		"it": "Impostazioni dell'adattatore per pi-hole",
+		"es": "Ajustes del adaptador para pi-hole",
+		"pl": "Ustawienia adaptera dla pi-hole",
+		"zh-cn": "pi-hole的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
